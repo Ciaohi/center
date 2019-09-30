fb03f73b1f964fa4d8ae14cac1fdd901c5619c95
ciaohi <465105975@qq.com>
2019-09-30 12:27:04 +0800

    transaction add

--- src/main/java/life/ciaohi/community/service/CommentService.java
+++ src/main/java/life/ciaohi/community/service/CommentService.java
@@ -10,6 +10,7 @@ import life.ciaohi.community.model.Comment;
 import life.ciaohi.community.model.Question;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class CommentService {
@@ -21,6 +22,7 @@ public class CommentService {
     @Autowired
     private QuestionExtMapper questionExtMapper;
 
+    @Transactional
     public void insert(Comment comment) {
         if(comment.getParentId()==null || comment.getParentId()==0){
             throw new CustomizeException(CustomizeErrorCode.TARGET_NOT_FOUND);
