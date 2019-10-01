d58434556b5b4e6fafb4aaf1c2c25f241f9cfe8e
ciaohi <465105975@qq.com>
2019-10-01 16:03:59 +0800

    reply list

--- src/main/java/life/ciaohi/community/controller/CommentController.java
+++ src/main/java/life/ciaohi/community/controller/CommentController.java
@@ -1,10 +1,9 @@
 package life.ciaohi.community.controller;
 
 
-import life.ciaohi.community.dto.CommentDTO;
+import life.ciaohi.community.dto.CommentCreateDTO;
 import life.ciaohi.community.dto.ResultDTO;
 import life.ciaohi.community.exceptin.CustomizeErrorCode;
-import life.ciaohi.community.mapper.CommentMapper;
 import life.ciaohi.community.model.Comment;
 import life.ciaohi.community.model.User;
 import life.ciaohi.community.service.CommentService;
@@ -13,8 +12,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.HashMap;
-import java.util.Map;
 
 @Controller
 public class CommentController {
@@ -24,16 +21,16 @@ public class CommentController {
 
     @ResponseBody
     @RequestMapping(value = "/comment",method= RequestMethod.POST)
-    public Object post(@RequestBody CommentDTO commentDTO,
+    public Object post(@RequestBody CommentCreateDTO commentCreateDTO,
                        HttpServletRequest request){
         User user=(User)request.getSession().getAttribute("user");
         if(user==null){
             return ResultDTO.errorOf(CustomizeErrorCode.NO_LOGIN);
         }
         Comment comment=new Comment();
-        comment.setParentId(commentDTO.getParentId());
-        comment.setContent(commentDTO.getContent());
-        comment.setType(commentDTO.getType());
+        comment.setParentId(commentCreateDTO.getParentId());
+        comment.setContent(commentCreateDTO.getContent());
+        comment.setType(commentCreateDTO.getType());
         comment.setGmtModified(System.currentTimeMillis());
         comment.setGmtCreate(System.currentTimeMillis());
         comment.setCommentator(user.getId());
