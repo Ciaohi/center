c8c5b171288d1dd197d52b1dac0fa451af0dddc1
ciaohi <465105975@qq.com>
2019-10-02 11:38:56 +0800

    reply better

--- src/main/java/life/ciaohi/community/controller/CommentController.java
+++ src/main/java/life/ciaohi/community/controller/CommentController.java
@@ -7,6 +7,7 @@ import life.ciaohi.community.exceptin.CustomizeErrorCode;
 import life.ciaohi.community.model.Comment;
 import life.ciaohi.community.model.User;
 import life.ciaohi.community.service.CommentService;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -26,6 +27,10 @@ public class CommentController {
         User user=(User)request.getSession().getAttribute("user");
         if(user==null){
             return ResultDTO.errorOf(CustomizeErrorCode.NO_LOGIN);
+        }
+
+        if(commentCreateDTO==null || StringUtils.isBlank(commentCreateDTO.getContent())){
+            return ResultDTO.errorOf(CustomizeErrorCode.CONTENT_IS_EMPTY);
         }
         Comment comment=new Comment();
         comment.setParentId(commentCreateDTO.getParentId());
